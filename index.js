a84e81c5f426cbaa26b88375f5752f757587cb77
Balthazar Gronon <git@balthazar.dev>
2020-10-20 09:17:06 -0700

    ZLP & CARB

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const list = require('./list')
 
 const PREFIX = 'https://unpkg.com/cryptoicons-cdn/images'
 const HAS_DARK = 2
+const IS_SVG = 3
 
 module.exports = (v, theme = 'light', fallback = true) => {
   const ticker = v && v.toUpperCase ? v.toUpperCase() : v
@@ -10,6 +11,7 @@ module.exports = (v, theme = 'light', fallback = true) => {
   }
 
   const suffix = list[ticker] === HAS_DARK && theme === 'dark' ? '-white' : ''
+  const ext = list[ticker] === IS_SVG ? 'svg' : 'png'
 
-  return `${PREFIX}/${ticker}${suffix}.png`
+  return `${PREFIX}/${ticker}${suffix}.${ext}`
 }
